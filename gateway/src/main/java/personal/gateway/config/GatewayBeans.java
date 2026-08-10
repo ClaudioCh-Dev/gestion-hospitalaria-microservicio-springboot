435be9df0ae9c0577eb711c5d9a2efac0702b1e1
Angelo <rideralucar@gmail.com>
2026-08-10 11:10:38 -0500

    delete implment route notification in gateway

--- gateway/src/main/java/personal/gateway/config/GatewayBeans.java
+++ gateway/src/main/java/personal/gateway/config/GatewayBeans.java
@@ -157,15 +157,6 @@ public class GatewayBeans {
                                                                                 .setFallbackUri("forward:/fallback?service=appointment-ms")))
                                                 .uri("lb://appointment-ms"))
                                 .route(route -> route
-                                                .path("/notification-ms/**")
-                                                .filters(filter -> filter
-                                                                .filter(this.authFilter)
-                                                                .circuitBreaker(config -> config
-                                                                                .setName("notification-circuitbreaker")
-                                                                                .setStatusCodes(Set.of("500"))
-                                                                                .setFallbackUri("forward:/fallback?service=notification-ms")))
-                                                .uri("lb://notification-ms"))
-                                .route(route -> route
                                                 .path("/billing-ms/**")
                                                 .filters(filter -> filter
                                                                 .filter(this.authFilter)
