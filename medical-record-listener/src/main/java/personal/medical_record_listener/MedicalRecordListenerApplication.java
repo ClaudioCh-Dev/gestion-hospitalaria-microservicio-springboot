acf56ea68033458a97af3e91585d84cdb6adda8d
Angelo <rideralucar@gmail.com>
2026-08-11 18:21:19 -0500

    init service record-listener

---
+++ medical-record-listener/src/main/java/personal/medical_record_listener/MedicalRecordListenerApplication.java
@@ -0,0 +1,13 @@
+package personal.medical_record_listener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalRecordListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalRecordListenerApplication.class, args);
+	}
+
+}
---
+++ medical-record-listener/src/test/java/personal/medical_record_listener/MedicalRecordListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.medical_record_listener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalRecordListenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
